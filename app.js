19123c745ef428da34972e62a3e6181824136c90
yovel_kamari <yovelkamari@gmail.com>
2026-01-20 18:02:11 +0200

    update

--- app.js
+++ app.js
@@ -36,4 +36,4 @@ app.use((req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT} update..`));
