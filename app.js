e86e1c482e4baa897d4ae11a95f3164cd8efac5c
yovel_kamari <yovelkamari@gmail.com>
2026-01-20 17:47:38 +0200

    defined dynamic port

--- app.js
+++ app.js
@@ -35,5 +35,5 @@ app.use((req, res) => {
   res.status(404).send("Not Found");
 });
 
-const PORT = 3000;
-app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
